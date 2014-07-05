df215229390b14c58fa1b1759da782e269576758
Tyler Holland <tyler@doubledutch.me>
2014-07-05 16:52:57 -0700

    Moved to Android Studio & Gradle

---
+++ app/build/generated/source/buildConfig/debug/com/tywholland/simpletimer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tywholland.simpletimer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.tywholland.simpletimer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "";
+}
